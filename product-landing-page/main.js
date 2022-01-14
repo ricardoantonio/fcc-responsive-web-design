bf16dab2f9d7d37c65193716bc6bb10ba1054b62
Ricardo Antonio <ricardoantoniomx@gmail.com>
2022-01-14 12:30:17 -0600

    Mobile Navigation

---
+++ product-landing-page/main.js
@@ -0,0 +1,16 @@
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    if (link.classList.contains("nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
